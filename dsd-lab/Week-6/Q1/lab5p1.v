e4845fbaa27b886cb85261d3d6a04aead41ea662
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2023-09-13 23:55:10 +0530

    Update and rename mux4to1.v to lab5p1.v

---
+++ dsd-lab/Week-6/Q1/lab5p1.v
@@ -0,0 +1,31 @@
+module mux4to1(w,s,f);
+input [3:0]w;
+input [1:0]s;
+output f;
+reg f;
+always @ (w or s)
+begin
+if (s==2'b00)
+    f = w[0];
+else if (s==2'b01)
+    f = w[1];
+else if (s==2'b10)
+    f = w[2];
+else (s==2'b11)
+    f = w[3];
+    
+    end
+endmodule
+
+module lab5p1(a,b,c,d,f);
+    input a,b,c,d;
+    output f;
+    wire [1:0]m;
+    wire [1:0]n;
+    assign m = {a , b};
+    assign n[0] = (~c & ~d);
+    assign n[1] = ~d;
+    assign n[2] = (~c & d);
+    assign n[3] = ~d;
+    mux4to1 stg0(m, n, f);
+endmodule
