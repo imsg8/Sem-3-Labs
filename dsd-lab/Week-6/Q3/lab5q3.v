7b252496ddd10d9f710554d753509f363f39ad63
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2023-09-14 09:57:20 +0530

    Update lab5q3.v

--- dsd-lab/Week-6/Q3/lab5q3.v
+++ dsd-lab/Week-6/Q3/lab5q3.v
@@ -6,17 +6,16 @@ module dec4to16(w, en, y);
    
    always @ (w or en)
       for (k=0;k<=15;k++)
-      {
-         if (w==k && en==1)
-            y[k] = 1;
-         else y[k] = 0;
-      }
+         if ((w==k) && (en==1))
+            	y[k] = 1;
+         else 
+        	y[k] = 0;
 endmodule
 
-         module lab5q3 (w,f);
-            input [3:0]w;
-            wire [0:15]y;
-            output f;
-            dec4to16 stg0(w[3:0], 1'b1, y[0:15]);
-            assign f = (y[1], y[4], y[6], y[8], y[9], y[13], y[15]);
-         endmodule
+ module lab5q3 (w,f);
+    input [3:0]w;
+    wire [0:15]y;
+    output f;
+    dec4to16 stg0(w[3:0], 1'b1, y[0:15]);
+    assign f = {y[1], y[4], y[6], y[8], y[9], y[13], y[15]};
+ endmodule
